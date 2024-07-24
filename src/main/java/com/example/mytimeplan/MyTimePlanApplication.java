3034310e6e6548401d29d01552a0461d347f4b82
Adi <adrianjach.lodz@gmail.com>
2024-07-24 17:11:31 +0200

    Fixed cache

--- src/main/java/com/example/mytimeplan/MyTimePlanApplication.java
+++ src/main/java/com/example/mytimeplan/MyTimePlanApplication.java
@@ -2,8 +2,10 @@ package com.example.mytimeplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MyTimePlanApplication {
 
 	public static void main(String[] args) {
